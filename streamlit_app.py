cfd7c3bf706cc94f3c633b9c09965dcf5d1e87b8
icheano <ivan_albestainku@hotmail.com>
2025-03-31 05:43:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -187,3 +187,54 @@ ax.pie(categoria_counts, labels=categoria_counts.index, autopct='%1.1f%%')
 ax.axis('equal')  # Para asegurar que el pastel es un círculo.
 
 st.pyplot(fig)
+
+
+
+
+
+
+
+
+
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Cargar el archivo Excel
+url = 'https://github.com/icheano/DemoVentas/raw/main/SalidaFinal.xlsx'
+df = pd.read_excel(url, sheet_name='Sheet1')
+
+# Título de la aplicación
+st.title('Filtros de Regiones y Estados')
+
+# Crear filtros de selección múltiple basados en las columnas 'Region' y 'State' y colocarlos en la barra lateral
+region_seleccionada = st.sidebar.multiselect(
+    'Selecciona una o más regiones',
+    options=df['Region'].unique(),
+    default=df['Region'].unique()
+)
+
+state_seleccionado = st.sidebar.multiselect(
+    'Selecciona uno o más estados',
+    options=df['State'].unique(),
+    default=df['State'].unique()
+)
+
+# Filtrar el DataFrame basado en la selección del usuario
+df_filtrado = df[(df['Region'].isin(region_seleccionada)) & (df['State'].isin(state_seleccionado))]
+
+# Mostrar el DataFrame filtrado
+st.write('DataFrame Filtrado:')
+st.dataframe(df_filtrado)
+
+# Crear una gráfica de pastel basada en la columna 'Category'
+st.write('Gráfica de Pastel por Categoría:')
+categoria_counts = df_filtrado['Category'].value_counts()
+
+# Mostrar la gráfica de pastel
+fig, ax = plt.subplots()
+ax.pie(categoria_counts, labels=categoria_counts.index, autopct='%1.1f%%')
+ax.axis('equal')  # Para asegurar que el pastel es un círculo.
+
+st.pyplot(fig)
