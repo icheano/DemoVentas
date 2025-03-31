6d04d7dd54d847501444a905ed3d2e0f49d97952
icheano <ivan_albestainku@hotmail.com>
2025-03-31 05:13:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -185,3 +185,25 @@ fig_pie = px.pie(category_counts,
                      names=category_counts.index, 
                      title='Distribución de Categorías')
 st.plotly_chart(fig_pie)
+# Aplica los filtros
+# Filtro para la columna "Region"
+  region_filter = st.selectbox("Selecciona una región:", df['Region'].unique())
+  df_filtered = df[df['Region'] == region_filter]
+
+# Segundo filtro para la columna "State" basado en el filtro de "Region"
+  state_filter = st.selectbox("Selecciona un estado:", df_filtered['State'].unique())
+  df_filtered = df_filtered[df_filtered['State'] == state_filter]
+
+
+
+
+# Muestra el DataFrame filtrado
+st.dataframe(df_filtered)
+
+# Gráfica de pastel para la columna "Category"
+category_counts = df_filtered['Category'].value_counts()
+fig_pie = px.pie(category_counts, 
+                     values=category_counts.values, 
+                     names=category_counts.index, 
+                     title='Distribución de Categorías')
+st.plotly_chart(fig_pie)
